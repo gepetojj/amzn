c9b433813e219c2b8f6dbd79f49426fb63731b70
João Pedro <nmjoaopedro22@gmail.com>
2024-09-03 09:10:40 -0300

    inicializa projeto

---
+++ src/main/java/application/amzn/AmznApplication.java
@@ -0,0 +1,11 @@
+package application.amzn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmznApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AmznApplication.class, args);
+	}
+}
