f8b2310a5e4b60a3007aea1743bc54688ec58949
João Pedro <nmjoaopedro22@gmail.com>
2024-09-03 09:11:38 -0300

    habilita cache na aplicação

--- src/main/java/application/amzn/AmznApplication.java
+++ src/main/java/application/amzn/AmznApplication.java
@@ -2,7 +2,9 @@ package application.amzn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class AmznApplication {
 	public static void main(String[] args) {
