0b4ea6794807098e4b6e6d5e596f92fc28e8130f
João Pedro <nmjoaopedro22@gmail.com>
2024-09-03 15:17:06 -0300

    implementa entidade Item

---
+++ src/main/java/application/amzn/entities/Item.java
@@ -0,0 +1,76 @@
+package application.amzn.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "items")
+public class Item implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Min(value = 0, message = "A quantidade não pode ser menor que zero.")
+    int quantity;
+
+    @Min(value = 0, message = "O preço não pode ser menor que zero.")
+    double price;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    Product product;
+
+    public Item() {
+    }
+
+    public Item(Long id, int quantity, double price, Product product) {
+        this.id = id;
+        this.quantity = quantity;
+        this.price = price;
+        this.product = product;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Min(value = 0, message = "A quantidade não pode ser menor que zero.")
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @Min(value = 0, message = "O preço não pode ser menor que zero.")
+    public double getPrice() {
+        return price;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(id, item.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", product=" + product +
+                '}';
+    }
+}
