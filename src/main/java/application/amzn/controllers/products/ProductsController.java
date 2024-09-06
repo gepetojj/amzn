76e9386275504e9060dfa681ae54c6318a76c3b2
João Pedro <nmjoaopedro22@gmail.com>
2024-09-06 11:01:29 -0300

    implementa rota de pesquisa de produto por id

--- src/main/java/application/amzn/controllers/products/ProductsController.java
+++ src/main/java/application/amzn/controllers/products/ProductsController.java
@@ -4,6 +4,7 @@ import application.amzn.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,14 @@ public class ProductsController {
     public ResponseEntity<?> getProducts() {
         var products = productRepository.findAll();
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProductById(@PathVariable("id") Long id) {
+        var product = productRepository.findById(id);
+        if (product.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
     }
 }
