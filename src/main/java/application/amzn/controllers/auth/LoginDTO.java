481459a8c119c05d612705bf67758c7366335b54
João Pedro <nmjoaopedro22@gmail.com>
2024-09-06 10:52:04 -0300

    adiciona validação nos DTOs

--- src/main/java/application/amzn/controllers/auth/LoginDTO.java
+++ src/main/java/application/amzn/controllers/auth/LoginDTO.java
@@ -1,4 +1,10 @@
 package application.amzn.controllers.auth;
 
-public record LoginDTO(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginDTO(
+        @NotBlank(message = "O email não deve estar em branco") @Email(message = "O email deve ser válido") String email,
+        @NotBlank(message = "A senha não deve estar em branco") @Size(min = 8, max = 64, message = "A senha deve ter entre 8 e 64 caracteres.") String password) {
 }
--- src/main/java/application/amzn/controllers/auth/RegisterDTO.java
+++ src/main/java/application/amzn/controllers/auth/RegisterDTO.java
@@ -1,4 +1,11 @@
 package application.amzn.controllers.auth;
 
-public record RegisterDTO(String name, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterDTO(
+        @NotBlank(message = "O nome não deve estar em branco") @Size(min = 4, max = 255, message = "O nome deve ter entre 4 e 255 caracteres.") String name,
+        @NotBlank(message = "O email não deve estar em branco") @Email(message = "O email deve ser válido") String email,
+        @NotBlank(message = "A senha não deve estar em branco") @Size(min = 8, max = 64, message = "A senha deve ter entre 8 e 64 caracteres.") String password) {
 }
