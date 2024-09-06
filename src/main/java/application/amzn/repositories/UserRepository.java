633c0ebb92f32ff46d23f42cc8e1a8d2c1e7fd2a
João Pedro <nmjoaopedro22@gmail.com>
2024-09-06 08:35:55 -0300

    adiciona método findByEmail no repositório de users

--- src/main/java/application/amzn/repositories/UserRepository.java
+++ src/main/java/application/amzn/repositories/UserRepository.java
@@ -2,5 +2,8 @@ package application.amzn.repositories;
 
 import application.amzn.entities.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public interface UserRepository extends CrudRepository<User, Long> { }
+public interface UserRepository extends CrudRepository<User, Long> {
+    UserDetails findByEmail(String email);
+}
