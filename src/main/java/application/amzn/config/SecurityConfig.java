c5b07602d8a815d2e64d0ab60720c3ec4fb589d3
João Pedro <nmjoaopedro22@gmail.com>
2024-09-06 11:56:33 -0300

    limita deleção de produtos apenas para admins

--- src/main/java/application/amzn/config/SecurityConfig.java
+++ src/main/java/application/amzn/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.DELETE, "/products/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
